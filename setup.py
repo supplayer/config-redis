bfc28afa4c8aceec8009dfabeae927811c371ad5
xingbo <xingbo@mozat.com>
2020-05-17 21:35:46 +0800

    change readme

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ with open("README.md") as fh:
 
 setuptools.setup(
     name="proj-config",
-    version="0.0.7",
+    version="0.0.8",
     author="RoyXing",
     author_email="x254724521@hotmail.com",
-    description="get proj config args from redis",
-    license='BSD',
+    description="Get proj config args from redis",
+    license='MIT',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="gitlab.com",
+    url="https://github.com/x254724521/proj_config",
     packages=setuptools.find_packages(exclude=('tests',)),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: BSD License",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
