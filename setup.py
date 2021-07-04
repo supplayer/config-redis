c405dce5f287eed7156df66613f9abf061263d83
supplayer <x254724521@hotmail.com>
2021-07-04 21:10:38 +0800

    optimize setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/x254724521/proj_config",
-    packages=setuptools.find_packages(exclude=('tests',)),
+    packages=setuptools.find_packages(include=('configredis',)),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
