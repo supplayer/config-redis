29143f46257868b885f9810922f2256930e8bddd
supplayer <x254724521@hotmail.com>
2021-07-04 21:09:13 +0800

    optimize setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="config-redis",
-    version="0.2.5",
-    author="RoyXing",
+    author="Supplayer",
     author_email="x254724521@hotmail.com",
     description="Get proj config args from redis",
     long_description=long_description,
@@ -19,5 +18,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
-    install_requires=['redis==3.5.2']
+    install_requires=['redis'],
+    setup_requires=['setuptools_scm'],
+    use_scm_version=True
 )
