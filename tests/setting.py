fcbad21f709d70c10680a2cceeb4ca536160df06
supplayer <x254724521@hotmail.com>
2021-10-13 10:12:34 +0800

    optimize configredis.setconf

--- tests/setting.py
+++ tests/setting.py
@@ -1,32 +1,34 @@
-from configredis.setconf import defaultconfig, devconfig, proconfig, configs, ConfigArgs, ConfigUpdate, lookup_proj_config, host_ip  # noqa
-from configredis.setredis import SetRedis
+from configredis.setconf import SetConfig, ConfigArgs, Tools
+# from configredis.setredis import SetRedis
 
 
 con = ConfigArgs()
 
-defaultconfig(
+SetConfig.defaultconfig(
     disk_name='TenD'
 )
 
-devconfig(
+SetConfig.devconfig(
     sentry=False,
     celery_broker="amqp://user:password@172.0.0.1:5672//",
-    r_flow={'host': host_ip('pro.cluster_redis.mztdns.com'), 'port': 7000, 'db': 0, "cluster": True}
+    r_flow={'host': Tools.host_ip('pro.cluster_redis.dns.com'), 'port': 7000, 'db': 0, "cluster": True},
+    r_flow1={'host': 'pro.cluster_redis.dns.com', 'port': 7000, 'db': 0, "cluster": True}
 )
 
-proconfig(
+SetConfig.proconfig(
     sentry=True,
     celery_broker="amqp://user:password@172.0.0.1:5672//",
     disk_name="TenB"
 )
 
-config = configs()  # if use ConfigUpdate.upsert_field_to_redis, need use configs for new fields
+config = SetConfig.configs('dns.com')  # if use ConfigUpdate.upsert_field_to_redis, need use configs for new fields
 
 
 if __name__ == '__main__':
     # ConfigUpdate.upsert_field_to_redis(disk_name='TenD')
-    print(configs())
+    print(config)
     # upsert_config_to_redis()  # update or insert current config to redis.
-    # print(lookup_proj_config())  # show current project config
+    # print(Tools.lookup_redis_proj_config())  # show current project config
+    # print(Tools.lookup_proj_config('dev'))
     # print(SetRedis.getkeys())
     # SetRedis.delfiels('config_redis')
