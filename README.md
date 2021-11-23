f6142870af39489df53ed6979058d6e7834dc508
supplayer <x254724521@hotmail.com>
2021-11-23 19:00:22 +0800

    optimize setconf.SetConfig and add new func print_config

--- README.md
+++ README.md
@@ -1,93 +1,104 @@
 # config-redis
 
-config-redis is a python library for get project config from redis and don't show your config details in the project.
+config-redis是一个python库，用于从redis获取项目配置，并且可以在项目中隐藏您的配置详细信息，以免配置信息泄露。
 
 ## Installation
 
-1.Use the package manager [pip](https://pypi.org/project/config-redis/) to install Proj_config.
+1.使用python包管理工具 [pip](https://pypi.org/project/config-redis/) 进行安装.
 
 ```bash
 pip install config-redis
 ```
 
-2.Install redis-server.
+2.本项目需要使用redis存储配置
 
-3.Set environment variables from command line.
+3.从命令行设置环境变量或写入配置文件
 
 ```bash
 export CONF_FOR_REDISCONF="{'host': '172.0.0.1', 'port': 6379, 'db': 0, 'password': 'your_password', 'decode_responses': True}" :$CONF_FOR_REDISCONF
 ```
 
-
-
 ## Usage
 
-#### 1.setup your config
-##### your_proj/setting.py
+#### 1.setup your config, e.g: your_proj/setting.py
 ```python
-from configredis.setconf import devconfig, proconfig, configs, ConfigArgs
+from configredis import SetConfig, Tools, SetRedis
 
-con = ConfigArgs()
+con = SetConfig.ConfigArgs()
 
-devconfig(
-    disk_name="Ten",
-    sentry=False,
-    celery_broker="amqp://user:password@0.0.0.0:5672//",
+# env 关键字是default, default是全局配置，可以放置env共用的配置
+SetConfig.defaultconfig(
+    c_env='default',
+    disk_name='TenD'
 )
 
-proconfig(
-    sentry=True,
+# env 关键字是dev
+SetConfig.devconfig(
+    c_env='dev',
     celery_broker="amqp://user:password@172.0.0.1:5672//",
+    r_flow={'host': Tools.host_ip('httpbin.org'), 'port': 7000, 'db': 0, "cluster": True},
+    r_flow1={'host': 'httpbin.org', 'port': 7000, 'db': 0, "cluster": True}
 )
 
-config = configs()
+# env 关键字是pro
+SetConfig.proconfig(
+    c_env='pro',
+    celery_broker="amqp://user:password@172.0.0.1:5672//",
+    disk_name="TenB"
+)
+
+# env 关键字是custom_env
+SetConfig.envconfig(
+    'custom_env',
+    c_env='custom_env'
+)
+
+config = SetConfig.configs('bin.org')  # 如果明文配置env，会以明文配置优先，如果配置文件删除明文配置，则会查找redis最新配置，SetConfig.configs会自动把明文配置持久化到redis, replace_domain参数可以自动解析相关域名IP，适合局域网静态IP地址，如果是动态IP，请关闭。
+if __name__ == '__main__':
+    print(config)  # 获得当前env的配置
+
 ```
 
-#### 2.Use upsert_config_to_redis func update or insert current config to redis.  lookup_proj_config func to check the config in redis.
-##### your_proj/setting.py
+#### 2.运行上一步SetConfig.configs会自动会自动把明文配置持久化到redis，此时删除所有明文配置文件SetConfig.defaultconfig/SetConfig.devconfig/SetConfig.proconfig/SetConfig.envconfig，项目会自动从redis拉取最新的配置。
 ```python
-from configredis.setconf import ConfigUpdate, lookup_proj_config
+from configredis import SetConfig, Tools, SetRedis
 
-ConfigUpdate.upsert_config_to_redis()  # update or insert current config to redis.
-
-print(lookup_proj_config())  # show current project config
-
-```
-
-#### 3.After write the project config to redis then your can chenge setting.py as below.
-##### your_proj/setting.py
-```python
-from configredis.setconf import devconfig, proconfig, configs, ConfigArgs
-
-con = ConfigArgs()
-
-devconfig(
-    disk_name=con['dev']['disk_name'],
-    sentry=con['dev']['sentry'],
-    celery_broker=con['dev']['celery_broker'],
-)
-
-proconfig(
-    sentry=con['pro']['sentry'],
-    celery_broker=con['pro']['celery_broker'],
-)
-
-config = configs()
+con = SetConfig.ConfigArgs()
+config = SetConfig.configs('bin.org')
 
 if __name__ == '__main__':
-    print(config)
+    print(config)  # 获得当前env的配置
+    # print(SetConfig.getconfs())  # 查看redis中项目目前所有版本配置
+    # SetConfig.print_config()  # 罗列项目所有版本的配置信息，并根据index打印相关配置，方便复制到配置文件, 顺序从新到旧
+    # print(con['custom_env'])  # 根据env查看redis储存的最新配置
+    # SetConfig.upsert_field_to_redis('custom_env', disk_name='TenC')  # 明文配置删除的情况下，给redis中的最新配置增加字段，不要跟明文配置同时使用，会导致redis版本错乱  # noqa
+    # print(Tools.lookup_redis_proj_config())  # 显示redis存储的最新配置
+    # print(Tools.lookup_proj_config(replace_domain='bin.org'))  # 显示当前所有env配置，明文优先于redis配置
+    # print(SetRedis().getkeys())  # 查看redis所有的项目名称
+    # SetConfig.delconfs()  # 罗列项目所有版本的配置信息，并根据index删除相关配置, 顺序从旧到新
+
+```
+
+#### 3.在项目中导入config获得对应的配置
+
+```python
+from tests.setting import config
+
+def test_env():
+    return config['celery_broker']
+
+if __name__ == '__main__':
+    print(test_env())
 ``` 
 
-#### 4.Run your project as config from command line.
+#### 4.使用命令行根据关键字获得配置运行项目。
 ```bash
-python sample.py pro/dev
+python sample.py pro/dev/custom_env  # default 是全局配置，可以放置env共用的配置
 ```
 
 
 ## Contributing
-Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
-
-Please make sure to update the tests as appropriate.
+使用前请做适当的测试，以确定跟您的项目完全兼容。
 
 ## License
 [MIT](https://choosealicense.com/licenses/mit/)
--- configredis/setconf.py
+++ configredis/setconf.py
@@ -7,7 +7,6 @@ import os
 import socket
 import re
 
-
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 redis = SetRedis()
@@ -48,14 +47,61 @@ class Tools:
         """
         lookup current project config.
         """
-        return SetConfig.with_domain(SetConfig.conf_mapping_, replace_domain, env)
+        conf_mapping_ = SetConfig.local_mapping_ or SetConfig.redis_mapping_
+        return SetConfig.with_domain(conf_mapping_, replace_domain, env)
 
 
 class ConfigUpdate:
     project_name_ = Tools.project_name()
     local_mapping_ = {}
     redis_mapping_ = Tools.lookup_redis_proj_config()
-    conf_mapping_ = local_mapping_ or redis_mapping_
+
+    @classmethod
+    def upsert_config_to_redis(cls, mapping=None, notify=True):
+        """
+        insert or update current config to redis.
+        """
+        redis.set_conf(cls.project_name_, mapping=mapping or cls.local_mapping_, notify=False)
+        logger.info(f" {cls.project_name_}'s config has been written to redis.") if notify else None
+
+    @classmethod
+    def upsert_field_to_redis(cls, env='default', notify=True, **kwargs):
+        conf_mapping_ = cls.local_mapping_ or cls.redis_mapping_
+        conf_mapping_[env] = {**conf_mapping_[env], **kwargs}
+        redis.set_conf(cls.project_name_, mapping=conf_mapping_, notify=False)
+        logger.info(f" {cls.project_name_}'s {env} config fileds has been written to redis.") if notify else None
+
+    @classmethod
+    def with_domain(cls, configs, replace_domain='', env=None):
+        configs = dumps(configs)
+        conf = loads(cls.__change_host(configs, replace_domain) if replace_domain else configs)
+        return {**conf.get('default', {}), **conf.get(env)} if env else conf
+
+    @classmethod
+    def __change_host(cls, conf, domain: str):
+        pattern = r'[\w\.]+' + domain.replace('.', r'\.')
+        conf, pattern = conf, re.compile(r'' + pattern)
+        n_d = {i: Tools.host_ip(i) for i in pattern.findall(conf)}
+        for k, v in n_d.items():
+            conf = conf.replace(k, v)
+        return conf
+
+    class ConfigArgs:
+        """
+        get config args value from redis.
+        """
+
+        def __getitem__(self, env):
+            """
+            sample:
+            :param env: name is 'dev'
+            :return: dict e.g: {'disk_name': 'TenTBc'}
+            """
+            return Tools.lookup_redis_proj_config().get(env, {})
+
+
+class SetConfig(ConfigUpdate):
+    __init_conf = ['default', 'dev', 'pro']
 
     @classmethod
     def defaultconfig(cls, **mapping):
@@ -85,109 +131,95 @@ class ConfigUpdate:
         cls.envconfig('pro', **mapping)
 
     @classmethod
-    def envconfig(cls, env: str, **mapping):
+    def envconfig(cls, env_: str, **mapping):
         """
         Set production env args.
         need import this func to your config document.
-        :param env: env name
+        :param env_: env name
         :param mapping: dict
         """
-        cls.local_mapping_[env] = mapping
+        cls.local_mapping_[env_] = mapping
 
     @classmethod
-    def with_domain(cls, configs, replace_domain='', env=None):
-        configs = dumps(configs)
-        conf = loads(cls.__change_host(configs, replace_domain) if replace_domain else configs)
-        return {**conf.get('default', {}), **conf.get(env)} if env else conf
-
-    @classmethod
-    def __change_host(cls, conf, domain: str):
-        pattern = r'[\w\.]+' + domain.replace('.', r'\.')
-        conf, pattern = conf, re.compile(r'' + pattern)
-        n_d = {i: Tools.host_ip(i) for i in pattern.findall(conf)}
-        for k, v in n_d.items():
-            conf = conf.replace(k, v)
-        return conf
-
-    class ConfigArgs:
-        """
-        get config args value from redis.
-        """
-
-        def __getitem__(self, env):
-            """
-            sample:
-            :param env: name is 'dev'
-            :return: dict e.g: {'disk_name': 'TenTBc'}
-            """
-            item = redis.getconf(Tools.project_name())
-            return item.get(env, '')
-
-
-class SetConfig(ConfigUpdate):
-
-    @classmethod
-    def upsert_config_to_redis(cls, mapping=None, notify=True):
-        """
-        insert or update current config to redis.
-        """
-        redis.set_conf(cls.project_name_, mapping=mapping or cls.local_mapping_, notify=False)
-        logger.info(f" {cls.project_name_}'s config has been written to redis.") if notify else None
-
-    @classmethod
-    def upsert_field_to_redis(cls, env='default', notify=True, **kwargs):
-        cls.conf_mapping_[env] = {**cls.conf_mapping_[env], **kwargs}
-        redis.set_conf(cls.project_name_, mapping=cls.conf_mapping_, notify=False)
-        logger.info(f" {cls.project_name_}'s {env} config fileds has been written to redis.") if notify else None
-
-    @classmethod
-    def configs(cls, replace_domain: str = None):
+    def configs(cls, replace_domain: str = None, on_redis=True):
         """
         get config info according env args. [defult/dev/pro]
         need import this func to your END of config document.
         :return: env config info as dict.
         """
         env = sys.argv[-1].lower()
-        cls.upsert_config_to_redis(cls.local_mapping_, notify=False)
         if not cls.local_mapping_:
             logger.warning(' Not catch local env args, will use redis env args.')
-        if env not in cls.conf_mapping_:
+        conf_mapping_ = cls.local_mapping_ or cls.redis_mapping_
+        if env not in conf_mapping_:
             logger.warning(' Not catch env args or defultconfig not setup. e.g: python sample.py dev|pro')
             logger.warning(' Will start under dev env? exit: ctrl+c')
-            logger.info(f' Current env is dev\n')
             env = 'dev'
-        else:
-            logger.info(f' Current env is {env}\n')
-        return cls.with_domain(cls.conf_mapping_, replace_domain, env)
+        logger.info(f' Current env is {env}\n')
+        if on_redis:
+            cls.upsert_config_to_redis(cls.local_mapping_, notify=False)
+        return cls.with_domain(conf_mapping_, replace_domain, env)
 
     @classmethod
-    def delconfs(cls, name=None):
+    def delconfs(cls, proj_name=None):
         """
         delete concent from name or key.
-        :param name: proj_name
+        :param proj_name: proj_name
         """
-        confs, res = cls.getconfs(name, score_cast_func=int), 0
+        proj_name = proj_name or cls.project_name_
+        confs, res = cls.getconfs(proj_name, score_cast_func=int), 0
+        del_items, choose = cls.__choose_items(confs)
+        if '0' in choose:
+            res = redis.redis.delete(proj_name)
+        elif del_items:
+            res = redis.redis.zrem(proj_name, *del_items)
+        if res:
+            logger.info(' Redis configs has been deleted.')
+
+    @classmethod
+    def getconfs(cls, proj_name=None, score_cast_func=None):
+        proj_name = proj_name or cls.project_name_
+        f_score = score_cast_func or cls.__f_score
+        return {str(i[1]): i[0] for i in redis.redis.zscan_iter(proj_name, score_cast_func=f_score)}
+
+    @classmethod
+    def print_config(cls, proj_name=None):
+        proj_name = proj_name or cls.project_name_
+        info = ''
+        confs = cls.getconfs(proj_name, score_cast_func=int)
+        print_items = cls.__choose_items(confs, stamp=True, reverse=True)[0]
+        for i in print_items:
+            info += f"\n\n>>{cls.__f_score(i)}<<\n\n" + "\n\n".join(
+                [cls.__f_env_key(k) + cls.__f_env_conf(v) for k, v in loads(confs[i]).items()]) + '\n'
+        print(f"{info}")
+
+    @classmethod
+    def indent(cls, num):
+        return ''.rjust(num, ' ')
+
+    @classmethod
+    def __choose_items(cls, confs, stamp=False, reverse=False):
         sore_keys = list(confs.keys())
-        sore_keys.sort()
-        p_conf = "\n".join(["0: All", *[f"{k+1}: {cls.__f_score(v)} ({v}) -> {confs[v]}"
+        sore_keys.sort(reverse=reverse)
+        p_conf = "\n".join(["0: All", *[f"{k + 1}: {cls.__f_score(v)} -> {confs[v]}"
                                         for k, v in enumerate(sore_keys)]])
         print(p_conf)
         choose = input('Enter index num which config choosed (split with ","):').replace(' ', '').split(',')
-        del_items = [confs[sore_keys[i]] for i in range(len(sore_keys)) if str(i+1) in choose]
-        if '0' in choose:
-            res = redis.redis.delete(name)
-        elif del_items:
-            res = redis.redis.zrem(name, *del_items)
-        print(res)
-        if res:
-            logger.info('Redis configs has been deleted.')
+        return [sore_keys[i] if stamp else confs[sore_keys[i]] for i in range(len(sore_keys)) if str(i + 1) in choose
+                ], choose
 
     @classmethod
-    def getconfs(cls, name=None, score_cast_func=None):
-        name = name or cls.project_name_
-        f_score = score_cast_func or cls.__f_score
-        return {str(i[1]): i[0] for i in redis.redis.zscan_iter(name, score_cast_func=f_score)}
+    def __f_env_key(cls, env_key):
+        return (f"SetConfig.{env_key}config(\n" if env_key in cls.__init_conf else
+                f"SetConfig.envconfig(\n{cls.indent(4)}'{env_key}',\n")
+
+    @classmethod
+    def __f_env_conf(cls, env_conf: dict):
+        for k, v in env_conf.items():
+            print(k, type(k), v, type(v))
+        return f'{cls.indent(4)}' + f'{cls.indent(4)}'.join(
+            [f'{k}="{v}",\n' if isinstance(v, str) else f'{k}={v},\n' for k, v in env_conf.items()]) + ")"
 
     @classmethod
     def __f_score(cls, stamp):
-        return datetime.fromtimestamp(int(stamp))
+        return f"{datetime.fromtimestamp(int(stamp))} ({stamp})"
