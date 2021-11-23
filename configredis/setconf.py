1dad6236720751c18bbd159a93efe7fbbbfcbcd1
supplayer <x254724521@hotmail.com>
2021-11-23 14:34:27 +0800

    optimize setconf.SetConfig

--- configredis/setconf.py
+++ configredis/setconf.py
@@ -1,15 +1,16 @@
 from configredis.setredis import SetRedis
 from json import dumps, loads
+from datetime import datetime
 import sys
 import logging
 import os
-import json
 import socket
 import re
 
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+redis = SetRedis()
 
 
 class Tools:
@@ -40,19 +41,21 @@ class Tools:
         :param proj_name: project name which lookup from redis.
         """
         proj_name = proj_name or cls.project_name()
-        return {k: json.loads(v) for k, v in SetRedis().getfiels(proj_name).items()}
+        return redis.getconf(proj_name)
 
     @classmethod
-    def lookup_proj_config(cls, env=None):
+    def lookup_proj_config(cls, env=None, replace_domain=''):
         """
         lookup current project config.
         """
-        return SetConfig.mapping_[env] if env else SetConfig.mapping_
+        return SetConfig.with_domain(SetConfig.conf_mapping_, replace_domain, env)
 
 
-class SetConfig:
+class ConfigUpdate:
     project_name_ = Tools.project_name()
-    mapping_ = {**{'default': {}, 'dev': {}, 'pro': {}}, **Tools.lookup_redis_proj_config()}
+    local_mapping_ = {}
+    redis_mapping_ = Tools.lookup_redis_proj_config()
+    conf_mapping_ = local_mapping_ or redis_mapping_
 
     @classmethod
     def defaultconfig(cls, **mapping):
@@ -61,7 +64,7 @@ class SetConfig:
         need import this func to your config document.
         :param mapping: dict
         """
-        cls.mapping_['default'] = mapping
+        cls.envconfig('default', **mapping)
 
     @classmethod
     def devconfig(cls, **mapping):
@@ -70,7 +73,7 @@ class SetConfig:
         need import this func to your config document.
         :param mapping: dict
         """
-        cls.mapping_['dev'] = mapping
+        cls.envconfig('dev', **mapping)
 
     @classmethod
     def proconfig(cls, **mapping):
@@ -79,7 +82,63 @@ class SetConfig:
         need import this func to your config document.
         :param mapping: dict
         """
-        cls.mapping_['pro'] = mapping
+        cls.envconfig('pro', **mapping)
+
+    @classmethod
+    def envconfig(cls, env: str, **mapping):
+        """
+        Set production env args.
+        need import this func to your config document.
+        :param env: env name
+        :param mapping: dict
+        """
+        cls.local_mapping_[env] = mapping
+
+    @classmethod
+    def with_domain(cls, configs, replace_domain='', env=None):
+        configs = dumps(configs)
+        conf = loads(cls.__change_host(configs, replace_domain) if replace_domain else configs)
+        return {**conf.get('default', {}), **conf.get(env)} if env else conf
+
+    @classmethod
+    def __change_host(cls, conf, domain: str):
+        pattern = r'[\w\.]+' + domain.replace('.', r'\.')
+        conf, pattern = conf, re.compile(r'' + pattern)
+        n_d = {i: Tools.host_ip(i) for i in pattern.findall(conf)}
+        for k, v in n_d.items():
+            conf = conf.replace(k, v)
+        return conf
+
+    class ConfigArgs:
+        """
+        get config args value from redis.
+        """
+
+        def __getitem__(self, env):
+            """
+            sample:
+            :param env: name is 'dev'
+            :return: dict e.g: {'disk_name': 'TenTBc'}
+            """
+            item = redis.getconf(Tools.project_name())
+            return item.get(env, '')
+
+
+class SetConfig(ConfigUpdate):
+
+    @classmethod
+    def upsert_config_to_redis(cls, mapping=None, notify=True):
+        """
+        insert or update current config to redis.
+        """
+        redis.set_conf(cls.project_name_, mapping=mapping or cls.local_mapping_, notify=False)
+        logger.info(f" {cls.project_name_}'s config has been written to redis.") if notify else None
+
+    @classmethod
+    def upsert_field_to_redis(cls, env='default', notify=True, **kwargs):
+        cls.conf_mapping_[env] = {**cls.conf_mapping_[env], **kwargs}
+        redis.set_conf(cls.project_name_, mapping=cls.conf_mapping_, notify=False)
+        logger.info(f" {cls.project_name_}'s {env} config fileds has been written to redis.") if notify else None
 
     @classmethod
     def configs(cls, replace_domain: str = None):
@@ -89,62 +148,46 @@ class SetConfig:
         :return: env config info as dict.
         """
         env = sys.argv[-1].lower()
-        if env not in cls.mapping_:
+        cls.upsert_config_to_redis(cls.local_mapping_, notify=False)
+        if not cls.local_mapping_:
+            logger.warning(' Not catch local env args, will use redis env args.')
+        if env not in cls.conf_mapping_:
             logger.warning(' Not catch env args or defultconfig not setup. e.g: python sample.py dev|pro')
             logger.warning(' Will start under dev env? exit: ctrl+c')
-            logger.info(f' Current env is dev')
-            return cls.__with_domain(replace_domain)
+            logger.info(f' Current env is dev\n')
+            env = 'dev'
         else:
-            logger.info(f' Current env is {env}')
-            return cls.__with_domain(replace_domain, env)
+            logger.info(f' Current env is {env}\n')
+        return cls.with_domain(cls.conf_mapping_, replace_domain, env)
 
     @classmethod
-    def __with_domain(cls, replace_domain='', env='dev'):
-        return cls.__change_host(
-            {**cls.mapping_[env], **cls.mapping_['default']}, replace_domain
-        ) if replace_domain else {**cls.mapping_[env], **cls.mapping_['default']}
+    def delconfs(cls, name=None):
+        """
+        delete concent from name or key.
+        :param name: proj_name
+        """
+        confs, res = cls.getconfs(name, score_cast_func=int), 0
+        sore_keys = list(confs.keys())
+        sore_keys.sort()
+        p_conf = "\n".join(["0: All", *[f"{k+1}: {cls.__f_score(v)} ({v}) -> {confs[v]}"
+                                        for k, v in enumerate(sore_keys)]])
+        print(p_conf)
+        choose = input('Enter index num which config choosed (split with ","):').replace(' ', '').split(',')
+        del_items = [confs[sore_keys[i]] for i in range(len(sore_keys)) if str(i+1) in choose]
+        if '0' in choose:
+            res = redis.redis.delete(name)
+        elif del_items:
+            res = redis.redis.zrem(name, *del_items)
+        print(res)
+        if res:
+            logger.info('Redis configs has been deleted.')
 
     @classmethod
-    def __change_host(cls, conf, domain: str):
-        pattern = r'[\w\.]+' + domain.replace('.', r'\.')
-        conf, pattern = dumps(conf), re.compile(r'' + pattern)
-        n_d = {i: Tools.host_ip(i) for i in pattern.findall(conf)}
-        for k, v in n_d.items():
-            conf = conf.replace(k, v)
-        return loads(conf)
-
-
-class ConfigArgs:
-    """
-    get config args value from redis.
-    """
-
-    def __getitem__(self, key):
-        """
-        sample:
-        con = SetRedis()
-        print(con['dev'])
-        :param key: name is 'dev'
-        :return: SetRedis().getfiels(name) -> dict e.g: {'disk_name': 'TenTBc'}
-        """
-        ConfigUpdate.upsert_config_to_redis(notify=True)
-        item = SetRedis().getfiels(Tools.project_name())
-        return json.loads(item.get(key)) if item.get(key) else ''
-
-
-class ConfigUpdate(SetConfig):
+    def getconfs(cls, name=None, score_cast_func=None):
+        name = name or cls.project_name_
+        f_score = score_cast_func or cls.__f_score
+        return {str(i[1]): i[0] for i in redis.redis.zscan_iter(name, score_cast_func=f_score)}
 
     @classmethod
-    def upsert_config_to_redis(cls, mapping=None, notify=True):
-        """
-        insert or update current config to redis.
-        """
-        SetRedis().upsert(cls.project_name_, mapping=mapping or cls.mapping_, notify=False)
-        logger.info(f"Project: {cls.project_name_}'s config has been written to redis.") if notify else None
-
-    @classmethod
-    def upsert_field_to_redis(cls, env='default', notify=True, **kwargs):
-        cls.mapping_[env] = {**cls.mapping_[env], **kwargs}
-        SetRedis().upsert(cls.project_name_, mapping=cls.mapping_, notify=False)
-        logger.info(
-            f"Project: {cls.project_name_}'s {env} config fileds has been written to redis.") if notify else None
+    def __f_score(cls, stamp):
+        return datetime.fromtimestamp(int(stamp))
--- configredis/setredis.py
+++ configredis/setredis.py
@@ -1,54 +1,58 @@
-import redis
+from time import time
+from json import loads, dumps
+from redis import ConnectionPool, StrictRedis
 import logging
 import os
-import json
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 class SetRedis:
-    def __init__(self):
-        self.__obj = redis.StrictRedis(**SetRedis.connetion())
+    def __init__(self, host=None, port=6379, db=0, password='mozat2014', decode_responses=True, **kwargs):
+        """
+        :param kwargs: redis config args.
+        """
+        self.__conf_redis = {**{'host': host, 'port': port, 'db': db, 'password': password},
+                             **kwargs} if host else {}
+        self.redis = StrictRedis(connection_pool=ConnectionPool(**{**self.connetion(), **self.__conf_redis}),
+                                 decode_responses=decode_responses)
 
-    @staticmethod
-    def connetion(conf_redis: dict = None):
+    def connetion(self):
         """
         get redis config form your environ or enter.
         redis config name use >CONF_FOR_REDISCONF<
-        :param conf_redis: redis config args.
         :return: dict of redis config
         """
         try:
-            redis_conf = conf_redis or eval(os.environ['CONF_FOR_REDISCONF'])
+            redis_conf = self.__conf_redis or eval(os.environ['CONF_FOR_REDISCONF'])
         except KeyError:
             os.environ['CONF_FOR_REDISCONF'] = input(
                 "WARNING:\n Haven't catch redis config from your env.\n Entry CONF_FOR_REDISCONF args：")
             redis_conf = eval(os.environ['CONF_FOR_REDISCONF'])
         return redis_conf
 
-    @staticmethod
-    def getkeys():
+    def getkeys(self):
         """
         get all fields from redis.
         :return: get fields(keys) list
         """
-        return SetRedis().__obj.keys()
+        return self.redis.keys()
 
-    @staticmethod
-    def getfiels(name):
+    def getconf(self, name):
         """
         get value according fields.
         :param name: redis fields
         :return: get keys and values according fields.
         """
-        try:
-            return SetRedis().__obj.hgetall(name)
-        except redis.exceptions.ResponseError:
-            return SetRedis().__obj.get(name)
+        res = self.redis.zrevrange(name, 0, 0)
+        if res:
+            return loads(self.redis.zrevrange(name, 0, 0)[0])
+        else:
+            return {}
 
-    @staticmethod
-    def upsert(name, mapping=None, notify=True, **kwargs):
+    def set_conf(self, name, mapping=None, notify=True, **kwargs):
         """
         create or update mapping according fields.
         :param notify: show log notice.
@@ -56,21 +60,7 @@ class SetRedis:
         :param mapping: <dict> data what is create or update
         """
         mapping = {**mapping, **kwargs} if mapping else kwargs
-        mapping = {k: json.dumps(v) for k, v in mapping.items()}
-        SetRedis().__obj.hmset(name, mapping)
-        logger.info(f"→{name}← has been setup the mapping: →{mapping}← done.") if notify else None
-
-    @staticmethod
-    def delfiels(name, *keys):
-        """
-        delete concent from name or key.
-        :param name: redis fields
-        :param keys: redis fields keys
-        :return: notice
-        """
-        if keys:
-            SetRedis().__obj.hdel(name, *keys)
-            logger.info(f"→{name}← has been delete →{keys}←.")
-        else:
-            SetRedis().__obj.delete(name)
-            logger.info(f"→{name}← and mapping delete already.")
+        if mapping:
+            mapping = {dumps(mapping): int(time())}
+            self.redis.zadd(name, mapping)
+            logger.info(f"→{name}← has been setup the mapping: →{mapping}← done.") if notify else None
